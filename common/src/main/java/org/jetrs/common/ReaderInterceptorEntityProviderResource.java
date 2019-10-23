9d98b97a4c0c514591d3427190ee480a0219590b
Seva Safris <seva@safris.org>
2019-10-23 15:49:06 +0800

    Update ReaderInterceptorEntityProviderResource

--- common/src/main/java/org/jetrs/common/ReaderInterceptorEntityProviderResource.java
+++ common/src/main/java/org/jetrs/common/ReaderInterceptorEntityProviderResource.java
@@ -1,4 +1,20 @@
-package org.jetrs.server;
+/* Copyright (c) 2018 JetRS
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * You should have received a copy of The MIT License (MIT) along with this
+ * program. If not, see <http://opensource.org/licenses/MIT/>.
+ */
+
+package org.jetrs.common;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.InvocationTargetException;
@@ -8,7 +24,7 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.ext.ReaderInterceptor;
 
 public class ReaderInterceptorEntityProviderResource extends EntityProviderResource<ReaderInterceptor> {
-  ReaderInterceptorEntityProviderResource(final Class<ReaderInterceptor> clazz, final ReaderInterceptor singleton) throws IllegalAccessException, InstantiationException, InvocationTargetException {
+  public ReaderInterceptorEntityProviderResource(final Class<ReaderInterceptor> clazz, final ReaderInterceptor singleton) throws IllegalAccessException, InstantiationException, InvocationTargetException {
     super(clazz, singleton, ReaderInterceptor.class);
   }
 
