093157d287c7b49386287dd6245a932c0a3b0591
Seva Safris <seva@safris.org>
2019-10-27 18:31:51 +0800

    Add copyright header [skip ci]

--- client/src/main/java/org/jetrs/client/InvocationImpl.java
+++ client/src/main/java/org/jetrs/client/InvocationImpl.java
@@ -1,7 +1,22 @@
+/* Copyright (c) 2019 JetRS
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * You should have received a copy of The MIT License (MIT) along with this
+ * program. If not, see <http://opensource.org/licenses/MIT/>.
+ */
+
 package org.jetrs.client;
 
 import java.io.IOException;
-import java.lang.reflect.Field;
 import java.net.CookieHandler;
 import java.net.CookieManager;
 import java.net.HttpCookie;
