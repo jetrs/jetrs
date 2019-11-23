ac12b73e38395c4caa1d1afac67662312118c43f
Seva Safris <seva@safris.org>
2019-11-23 15:11:09 +0800

    Support Range header

--- common/src/main/java/org/jetrs/common/ext/provider/FileProvider.java
+++ common/src/main/java/org/jetrs/common/ext/provider/FileProvider.java
@@ -20,6 +20,7 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.RandomAccessFile;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Type;
 import java.nio.file.Files;
@@ -31,11 +32,87 @@ import javax.ws.rs.ext.MessageBodyReader;
 import javax.ws.rs.ext.MessageBodyWriter;
 import javax.ws.rs.ext.Provider;
 
+import org.libj.util.function.BiObjBiLongConsumer;
+import org.libj.util.function.Throwing;
+
 /**
  * JAX-RS 2.1 Section 4.2.4
  */
 @Provider
 public class FileProvider implements MessageBodyReader<File>, MessageBodyWriter<File> {
+  static void writeTo(final Object header, final File file, final OutputStream out, final BiObjBiLongConsumer<RandomAccessFile,OutputStream> consumer) throws IOException {
+    final String range;
+    final int start;
+    if (header == null || (range = String.valueOf(header).trim()).length() == 0 || (start = range.indexOf("bytes=")) == -1) {
+      Files.copy(file.toPath(), out);
+      return;
+    }
+
+    try (final RandomAccessFile raf = new RandomAccessFile(file, "r")) {
+      parseRange(range, start + 6, raf, out, consumer);
+    }
+  }
+
+  private static void parseRange(final String range, int i, final RandomAccessFile raf, final OutputStream out, final BiObjBiLongConsumer<RandomAccessFile,OutputStream> consumer) {
+    final StringBuilder builder = new StringBuilder();
+    long from = Long.MIN_VALUE;
+    long to = Long.MAX_VALUE;
+    final int len = range.length();
+    for (char ch; i <= len; ++i) {
+      if (i == len || (ch = range.charAt(i)) == ',') {
+        if (builder.length() > 0) {
+          if (from != Long.MIN_VALUE)
+            to = Long.valueOf(builder.toString());
+          else
+            from = -Long.valueOf(builder.toString());
+        }
+
+        if (from <= to && (from != Long.MIN_VALUE || to != Long.MAX_VALUE))
+          consumer.accept(raf, out, from, to);
+
+        from = Long.MIN_VALUE;
+        to = Long.MAX_VALUE;
+        builder.setLength(0);
+      }
+      else if (ch == '-') {
+        if (from != Long.MIN_VALUE) {
+          to = Long.valueOf(builder.toString());
+          builder.setLength(0);
+        }
+        else if (builder.length() > 0) {
+          from = Long.valueOf(builder.toString());
+          builder.setLength(0);
+        }
+      }
+      else if (Character.isDigit(ch)) {
+        builder.append(ch);
+      }
+    }
+  }
+
+  private static final BiObjBiLongConsumer<RandomAccessFile,OutputStream> consumer = new BiObjBiLongConsumer<RandomAccessFile,OutputStream>() {
+    @Override
+    public void accept(final RandomAccessFile raf, final OutputStream out, final long from, final long to) {
+      try {
+        final long len;
+        if (from < 0) {
+          raf.seek(raf.length() + from);
+          len = -from;
+        }
+        else {
+          raf.seek(from);
+          len = to == Long.MAX_VALUE ? raf.length() - from : to;
+        }
+
+        for (int i = 0; i < len; ++i)
+          out.write(raf.read());
+      }
+      catch (final IOException e) {
+        Throwing.rethrow(e);
+      }
+    }
+  };
+
   @Override
   public boolean isReadable(final Class<?> type, final Type genericType, final Annotation[] annotations, final MediaType mediaType) {
     return File.class.isAssignableFrom(type);
@@ -60,6 +137,6 @@ public class FileProvider implements MessageBodyReader<File>, MessageBodyWriter<
 
   @Override
   public void writeTo(final File t, final Class<?> type, final Type genericType, final Annotation[] annotations, final MediaType mediaType, final MultivaluedMap<String,Object> httpHeaders, final OutputStream entityStream) throws IOException {
-    Files.copy(t.toPath(), entityStream);
+    writeTo(httpHeaders.getFirst("Range"), t, entityStream, consumer);
   }
 }
